d4ddb1f51dd1d7798eb89c7464d95da49ad5113c
sergio <granrah1@gmail.com>
2023-07-20 11:52:43 -0400

    arreglo de dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,17 +1,57 @@
-    <script setup>
-    
-    
-    
-    
-    </script>
+  <script setup>
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
+  
+  const cards = ref([
+    {
+      title: 'Calendario General',
+      description: 'Gestiona tus eventos, edítalos, crea clientes a partir de aquí si gustas, etc.',
+      route: 'calendario-general',
+      icono: 'mdi-calendar-clock-outline'
+    },
+    {
+      title: 'Cobros Pendientes',
+      description: 'Aquí encontrarás a los clientes con deuda, podrás además ir al costo asociado y editarlo',
+      route: 'cobros-pendientes',
+      icono: 'mdi-account-cash'
+    }
+  ]);
+  
+  const router = useRouter();
+  
+  const goToRoute = (name) => {
+    router.push({name: name});
+  };
+  </script>
 <template>
-   <h1>Dashboard</h1>
-
-    <v-btn :to="{name: 'calendario-general'}">Calendario General</v-btn>
-    <v-btn :to="{name: 'cobros-pendientes'}">Cobros Pendientes</v-btn>
-</template>
-
-
-<style lang="scss" scoped>
-
-</style>
+    <v-container class="dashboard-container" fluid>
+      <v-row justify="center">
+        <v-col cols="12" md="6" lg="4" v-for="card in cards" :key="card.title">
+          <v-card class="dashboard-card" @click="goToRoute(card.route)">
+            <v-card-title class="bg-indigo">
+            <v-icon class="mr-3">{{ card.icono }}</v-icon>  {{ card.title }}
+            </v-card-title>
+            <v-card-text>
+              {{ card.description }}
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  
+  <style scoped>
+  .dashboard-container {
+    padding: 20px;
+  }
+  
+  .dashboard-card {
+    cursor: pointer;
+    margin-bottom: 20px;
+  }
+  
+ 
+  </style>
+<!-- <v-btn :to="{name: 'calendario-general'}">Calendario General</v-btn>
+<v-btn :to="{name: 'cobros-pendientes'}">Cobros Pendientes</v-btn> -->
