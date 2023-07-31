1beb11ef742dd248d088fac16deaf959474a88f3
sergio <granrah1@gmail.com>
2023-07-31 01:07:51 -0400

    excluir de proximas atenciones de los clientes que ya no existen

--- src/stores/proximasAtenciones.js
+++ src/stores/proximasAtenciones.js
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia';
-import { collection, getDocs } from 'firebase/firestore';
+import { getDocs, collection, deleteDoc, doc as Doc } from 'firebase/firestore';
+
 import { useFirestore } from 'vuefire';// Asegúrate de tener una referencia a la base de datos de Firebase aquí
 import {ref, computed }from 'vue';
 import { useRouter } from 'vue-router';
@@ -14,19 +15,35 @@ export const UseProximasAtencionesStore = defineStore('proximas-atenciones',()=>
     const proximasAtenciones = ref([])
     const searchTerm = ref('')
 
-    async function  fetchProximasAtenciones() {
-        try {
-          const querySnapshot = await getDocs(collection(db, 'ProximasAtenciones'));
-          proximasAtenciones.value = querySnapshot.docs.map(doc => ({
-              id: doc.id, // Agregar el ID del documento al objeto
-              ...doc.data(), // Agregar los datos del documento al objeto
-            }));
-  
-        } catch (error) {
-          console.error('Error al obtener datos de Firebase:', error);
-        }
-      }
 
+    async function fetchProximasAtenciones() {
+      try {
+      // OBTENGO IDS DE CLIENTES
+        const clientesSnapshot = await getDocs(collection(db, 'clientes'));
+        const idsClientesExistentes = clientesSnapshot.docs.map(doc => doc.id);
+    
+        // /OBTENGO PRXIMAS ATENCUONES Y FILTRO POR IDS DE CLIENTES
+        const proximasAtencionesSnapshot = await getDocs(collection(db, 'ProximasAtenciones'));
+        proximasAtenciones.value = proximasAtencionesSnapshot.docs.map(doc => {
+          const datosProximaAtencion = doc.data();
+          if (idsClientesExistentes.includes(datosProximaAtencion.idCliente)) {
+            return {
+              id: doc.id,
+              ...datosProximaAtencion,
+            };
+          } else {
+          //  ELIMINO LA PROXIMA ATENCION QUE HAGA MATCH CON EL ID DE CLIENTE QUE YA NO EXISTE 
+            deleteDoc(Doc(db, 'ProximasAtenciones', doc.id)).catch(error => {
+              console.error('Error al eliminar próxima atención:', error);
+            });
+            return null; //RETORNO NULL PARA EXCLUIRLO DE LA LISTA
+          }
+        }).filter(Boolean); // FILTRO LOS CLIENTES NULOS QUE NO EXISTEN DE LA LISTA
+    
+      } catch (error) {
+        console.error('Error al obtener datos de Firebase:', error);
+      }
+    }
       const semanasRestantes = computed(()=>{
 
         return (fechaProximaMantencion) => {
